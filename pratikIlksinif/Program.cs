9e82c1ce199f56d32dcef8db1ef5d87fc6d2697a
emreyilmaz <emreyilmazxy@gmail.com>
2025-05-17 16:35:32 +0300

    Add project files.

---
+++ pratikIlksinif/Program.cs
@@ -0,0 +1,33 @@
+﻿using pratikIlksinif;
+
+namespace kisiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person ogrenci1 = new Person();
+            ogrenci1.ad = "Ali";
+            ogrenci1.soyad = "Yılmaz";
+            ogrenci1.dogumTarihi = "24.03.1993";
+            
+            Person ogrenci2 = new Person();
+            ogrenci2.ad = "Ayşe";
+            ogrenci2.soyad = "Yılmaz";
+            ogrenci2.dogumTarihi = "15.03.1998";
+            
+            Person ogretmen = new Person();
+            ogretmen.ad = "mehmet";
+            ogretmen.soyad = "Yılmaz";
+            ogretmen.dogumTarihi = "12.03.1980";
+
+            Console.WriteLine($"ilk öğrenci bilgileri ad: {ogrenci1.ad} soyad:{ogrenci1.soyad} doğum tarihi:{ogrenci1.dogumTarihi} ");
+           
+            Console.WriteLine($"ikinci öğrenci bilgileri ad: {ogrenci2.ad} soyad:{ogrenci2.soyad} doğum tarihi:{ogrenci2.dogumTarihi} ");
+            
+            Console.WriteLine($" öğretmen bilgileri ad: {ogretmen.ad} soyad:{ogretmen.soyad} doğum tarihi:{ogretmen.dogumTarihi} ");
+            
+
+        }
+    }
+}
---
+++ pratikIlksinif/person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pratikIlksinif
+{
+    public class Person
+    {
+        private string _ad;
+        private string _soyad;
+        private string _dogumTarihi;
+
+        public string ad
+        {
+            get { return _ad; }
+            set { _ad = value; }
+        }
+        public string soyad
+        {
+            get { return _soyad; }
+            set { _soyad = value; }
+        }
+        public string dogumTarihi
+        {
+            get { return _dogumTarihi; }
+            set { _dogumTarihi = value; }
+        }
+        
+    }
+}
